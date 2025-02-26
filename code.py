1e5d78e4f658fec97aba18d3c485ecb58cc4336e
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2025-02-25 19:53:07 -0500

    save code

--- code.py
+++ code.py
@@ -9,13 +9,15 @@ This module is a Raspberrypy Pico program that causes a LED to blink on for one 
 import board
 import digitalio
 import time
+import busio
 
 # varaibles
-blink_delay
+blink_delay = 1
 
+print("hello world")
 
 # setup
-led = digitalio.DigitalInOut(board.GP3)
+led = digitalio.DigitalInOut(board.GP13)
 led.direction = digitalio.Direction.OUTPUT
 
 # loop
